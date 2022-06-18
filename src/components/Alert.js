5e2a3ae4d36dad32b39d1599aee700793047f529
SahilChandravanshi <sahilcdelhi@gmail.com>
2022-06-18 07:30:02 +0530

    cls fixed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,18 +4,18 @@ function Alert(props) {
   const capitalize = (word) => {
     const lower = word.toLowerCase();
     return lower.charAt(0).toUpperCase() + lower.slice(1);
-  }
+  };
   return (
-    props.alert && (
-      <div>
+    <div style={{ height: "50px" }}>
+      {props.alert && (
         <div
           className={`alert alert-${props.alert.type} alert-dismissible fade show`}
           role="alert"
         >
           {capitalize(props.alert.type)} : {props.alert.message}
         </div>
-      </div>
-    )
+      )}
+    </div>
   );
 }
 
